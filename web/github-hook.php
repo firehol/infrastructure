f2487eeec8cb0cfa80160bba636699cf307b41dd
FireHOL Infrastructure User <firehol@firehol.org>
2013-10-16 17:31:22 +0200

    Include URL so we can identify repository

--- web/github-hook.php
+++ web/github-hook.php
@@ -10,18 +10,25 @@ if ($ref == "") {
 }
 
 $pusher = $result->{"pusher"};
+$repository = $result->{"repository"};
+$url = $repository->{"url"};
 $email = $pusher->{"email"};
 $action = "build";
 
-$filename = preg_replace('/[^A-Za-z0-9_-]/', '-', $ref);
+print "URL: $url\n";
 print "Ref: $ref\n";
 print "Email: $email\n";
 print "Action: build\n";
 
-$fh = fopen("/home/firehol/web/requests/req.$filename", "w");
+$parturl = preg_replace('/.*github.com./', '', $url);
+$fileurl = preg_replace('/[^A-Za-z0-9_-]/', '-', $parturl);
+$fileref = preg_replace('/[^A-Za-z0-9_-]/', '-', $ref);
+
+$fh = fopen("/home/firehol/web/requests/req.$fileurl.$fileref", "w");
 if (!$fh) {
   exit(1);
 }
+fwrite($fh, "URL: $url\n");
 fwrite($fh, "Ref: $ref\n");
 fwrite($fh, "Email: $email\n");
 fwrite($fh, "Action: build\n");
